99912c4ad45eb0bc238f2964b16e45a6978b4449
Nicolle Policiano <nicolle.fabri@gmail.com>
2023-09-09 08:40:19 -0500

    Remove the unused alertIsVisible property

--- week2/starter/ColorPicker/ContentView.swift
+++ week2/starter/ColorPicker/ContentView.swift
@@ -33,7 +33,6 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
   @State private var redColor: Double = 0.0
   @State private var greenColor: Double = 0.0
   @State private var blueColor: Double = 0.0
