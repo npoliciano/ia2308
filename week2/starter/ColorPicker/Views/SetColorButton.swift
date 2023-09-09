d8f1398d9ec9a84109e3d3d3f6648803c151b6ef
Nicolle Policiano <nicolle.fabri@gmail.com>
2023-09-09 11:44:24 -0500

    Place views in separate files

--- week2/starter/ColorPicker/Views/SetColorButton.swift
+++ week2/starter/ColorPicker/Views/SetColorButton.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,37 +32,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
+struct SetColorButton: View {
+  var redColor: Double
+  var greenColor: Double
+  var blueColor: Double
+  @Binding var foregroundColor: Color
   
   var body: some View {
-    
-    VStack(spacing: 16) {
-      TitleView(title: "Color Picker")
-
-      ColorDisplayView(color: foregroundColor)
-      
-      SliderView(title: "Red", value: $redColor)
-        .tint(.red)
-      SliderView(title: "Green", value: $greenColor)
-        .tint(.green)
-      SliderView(title: "Blue", value: $blueColor)
-        .tint(.blue)
-  
-      SetColorButton(redColor: redColor, greenColor: greenColor, blueColor: blueColor, foregroundColor: $foregroundColor)
+    Button("Set Color") {
+      foregroundColor = Color(red: redColor / 255, green: greenColor / 255, blue: blueColor / 255)
     }
-    .padding(24)
+    .font(.headline)
+    .bold()
+    .foregroundColor(Color.white)
+    .frame(height: 24)
+    .padding()
+    .background(Color.blue)
+    .cornerRadius(16.0)
+    .overlay(
+      RoundedRectangle(cornerRadius: 16.0)
+        .stroke(Color.white, lineWidth: 2)
+    )
   }
 }
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    ContentView()
-      .preferredColorScheme(.dark)
-      .previewDisplayName("Dark")
-  }
+struct SetColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+      SetColorButton(redColor: 50, greenColor: 100, blueColor: 200, foregroundColor: .constant(.green))
+    }
 }
